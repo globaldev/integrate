26bba18a85686f8bdd9be339b92cbab93782a78b
Rob Young <robyoung26@gmail.com>
2012-04-18 22:31:27 +0100

    Simple MessageHandler registration.

--- lib/integrate/dispatcher/dispatcher.rb
+++ lib/integrate/dispatcher/dispatcher.rb
@@ -1,9 +1,21 @@
 module Integrate
   class Dispatcher
     
-    # Registered Message Handlers
-    attr_accessor :handlers
+    def initialize
+      # Registered message handlers
+      @handlers = {}
+    end
     
+    def handlers
+      @handlers.dup
+    end
     
+    def register_handler(handler)
+      @handlers[handler.hash] = handler
+    end
+    
+    def unregister_handler(handler)
+      @handlers.delete(handler.hash)
+    end
   end
 end
--- test/integrate/dispatcher/dispatcher_test.rb
+++ test/integrate/dispatcher/dispatcher_test.rb
@@ -7,5 +7,31 @@ module Integrate
       dispatcher = Dispatcher.new
       refute_nil(dispatcher)
     end
+    
+    def test_register_message_handler
+      dispatcher = Dispatcher.new
+      dummy_handler = DummyMessageHandler.new
+      dispatcher.register_handler(dummy_handler)
+      assert_equal({dummy_handler.hash => dummy_handler}, dispatcher.handlers)
+    end
+    
+    def test_remove_message_handler
+      dispatcher = Dispatcher.new
+      handler_one = DummyMessageHandler.new
+      handler_two = DummyMessageHandler.new
+      
+      dispatcher.register_handler(handler_one)
+      dispatcher.register_handler(handler_two)
+      
+      assert_equal({handler_one.hash => handler_one, handler_two.hash => handler_two}, dispatcher.handlers)
+      
+      dispatcher.unregister_handler(handler_one)
+      assert_equal({handler_two.hash => handler_two}, dispatcher.handlers)
+      
+    end
+    
+    class DummyMessageHandler
+    end
+    
   end
 end
