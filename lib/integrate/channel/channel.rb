0647d5f3e48c90984877897caf0db3318599cedb
Matthew Sadler <mat@sourcetagsandcodes.com>
2012-05-01 11:51:26 +0100

    prefer accessor methods over instance variables

--- lib/integrate/channel/channel.rb
+++ lib/integrate/channel/channel.rb
@@ -10,16 +10,16 @@ module Integrate
     end
     
     def subscribe(message_handler)
-      @dispatcher.register_handler(message_handler)
+      dispatcher.register_handler(message_handler)
       # TODO Log handler registration with count.
     end
     
     def unsubscribe(message_handler)
-      @dispatcher.unregister_handler(message_handler)
+      dispatcher.unregister_handler(message_handler)
     end
     
     def send(message)
-      @dispatcher.call(message)
+      dispatcher.call(message)
     end
   end
 end
